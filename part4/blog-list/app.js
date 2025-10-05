0bfa270c56e1219df0e24cc41d15a062b656a5f4
amir13841661 <bdya277@gmail.com>
2025-10-05 20:03:35 +0330

    update:finished exercise 4.2 (refactoring the app)

---
+++ part4/blog-list/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const blogsRouter = require("./controllers/blogs");
+const app = express();
+const config = require("./utils/config");
+const mongoose = require("mongoose");
+const middleware = require("./utils/middleware");
+const logger = require("./utils/logger");
+
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogsRouter);
+
+mongoose
+  .connect(config.MONGODB_URL)
+  .then(() => {
+    logger.info("connected to MongoDB");
+  })
+  .catch((error) => {
+    logger.error("error connection to MongoDB:", error.message);
+  });
+
+app.use(middleware.unknownEndpoint);
+
+module.exports = app;
--- part4/blog-list/index.js
+++ part4/blog-list/index.js
@@ -1,38 +1,10 @@
+const app = require("./app");
+const config = require("./utils/config");
 const express = require("express");
-const mongoose = require("mongoose");
-
-const app = express();
-
-const blogSchema = mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
-});
-
-const Blog = mongoose.model("Blog", blogSchema);
-
-const mongoUrl =
-  "mongodb+srv://amir13841661:yazd1384@test.rfnaazf.mongodb.net/?retryWrites=true&w=majority&appName=test";
-mongoose.connect(mongoUrl);
+const logger = require("./utils/logger");
 
 app.use(express.json());
 
-app.get("/api/blogs", (request, response) => {
-  Blog.find({}).then((blogs) => {
-    response.json(blogs);
-  });
-});
-
-app.post("/api/blogs", (request, response) => {
-  const blog = new Blog(request.body);
-
-  blog.save().then((result) => {
-    response.status(201).json(result);
-  });
-});
-
-const PORT = 3003;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+app.listen(config.PORT, () => {
+  console.log(`Server running on port ${config.PORT}`);
 });
