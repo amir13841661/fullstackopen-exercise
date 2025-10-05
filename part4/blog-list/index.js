b3f0a0a9a91241a8c72f8655d102353842a30c3a
amir13841661 <bdya277@gmail.com>
2025-10-05 12:54:43 +0330

    update:finished exercise 4.1

---
+++ part4/blog-list/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const app = express();
+
+const blogSchema = mongoose.Schema({
+  title: String,
+  author: String,
+  url: String,
+  likes: Number,
+});
+
+const Blog = mongoose.model("Blog", blogSchema);
+
+const mongoUrl =
+  "mongodb+srv://amir13841661:yazd1384@test.rfnaazf.mongodb.net/?retryWrites=true&w=majority&appName=test";
+mongoose.connect(mongoUrl);
+
+app.use(express.json());
+
+app.get("/api/blogs", (request, response) => {
+  Blog.find({}).then((blogs) => {
+    response.json(blogs);
+  });
+});
+
+app.post("/api/blogs", (request, response) => {
+  const blog = new Blog(request.body);
+
+  blog.save().then((result) => {
+    response.status(201).json(result);
+  });
+});
+
+const PORT = 3003;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
