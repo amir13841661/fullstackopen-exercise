54e826d6e39898b832cdacf06167e5bc06599c04
amir13841661 <bdya277@gmail.com>
2025-10-06 10:22:28 +0330

    update:finished exercises 4.5-4.7

--- part4/blog-list/utils/list_helper.js
+++ part4/blog-list/utils/list_helper.js
@@ -1,3 +1,5 @@
+const _ = require("lodash");
+
 const dummy = (blogs) => {
   return 1;
 };
@@ -6,7 +8,45 @@ const totalLikes = (blogs) => {
   return blogs.reduce((sum, blog) => sum + blog.likes, 0);
 };
 
+const favoriteBlog = (blogs) => {
+  if (blogs.length === 0) {
+    return null;
+  }
+  return blogs.reduce((max, blog) => (blog.likes > max.likes ? blog : max));
+};
+
+const mostBlogs = (blogs) => {
+  if (blogs.length == 0) {
+    return {};
+  }
+  const authorWithMostBlogs = _(blogs)
+    .groupBy("author")
+    .map((blogs, author) => ({
+      author,
+      blogs: blogs.length,
+    }))
+    .maxBy("blogs");
+  return authorWithMostBlogs;
+};
+
+const mostLikes = (blogs) => {
+  if (blogs.length == 0) {
+    return {};
+  }
+  const authorWithMostLikes = _(blogs)
+    .groupBy("author")
+    .map((blogs, author) => ({
+      author,
+      totalLikes: _.sumBy(blogs, "likes"),
+    }))
+    .maxBy("totalLikes");
+
+  return authorWithMostLikes;
+};
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog,
+  mostBlogs,
+  mostLikes,
 };
